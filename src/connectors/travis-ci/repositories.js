1b78d76eee2c23f5bbe46e37ccdfc7f8f057eae1
Josh Black <joshuablack.work@gmail.com>
2018-01-03 11:49:49 -0600

    Add initial travis connectors (#4)
    * chore: add TravisCI status badge
    
    * feat: add initial connectors for TravisCI

---
+++ src/connectors/travis-ci/repositories.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const request = require('request-promise');
+const page = require('./tools/page');
+
+const { TRAVIS_TOKEN } = process.env;
+const baseConfig = {
+  baseUrl: 'https://api.travis-ci.org',
+  headers: {
+    Accept: 'application/json',
+    Authorization: `token ${TRAVIS_TOKEN}`,
+    'Content-Type': 'application/json',
+    'User-Agent': 'carbon-bot',
+    'Travis-API-Version': 3,
+  },
+  json: true,
+  timeout: 10000,
+};
+
+async function all(owner, qs = {}) {
+  const config = {
+    ...baseConfig,
+    uri: `/owner/${owner}/repos`,
+    method: 'GET',
+    qs,
+  };
+
+  return await page('repositories', config);
+}
+
+async function activate(slug) {
+  const config = {
+    ...baseConfig,
+    uri: `/repo/${encodeURIComponent(slug)}/activate`,
+    method: 'POST',
+  };
+  return await request(config);
+}
+
+async function deactivate(slug) {
+  const config = {
+    ...baseConfig,
+    uri: `/repo/${encodeURIComponent(slug)}/deactivate`,
+    method: 'POST',
+  };
+  return await request(config);
+}
+
+module.exports = exports = {
+  all,
+  activate,
+  deactivate,
+};
+
+exports.all = all;
+exports.activate = activate;
+exports.deactivate = deactivate;
